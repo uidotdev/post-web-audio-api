94378ad526b6b94fb80f8ac64846e048b84015a6
Alex Anderson <alexanderson1993@gmail.com>
2020-11-24 12:04:27 -0500

    Add a snare button with a highpass filter

--- src/index.js
+++ src/index.js
@@ -29,3 +29,20 @@ button.addEventListener("click", () => {
   whiteNoiseSource.start();
 });
 document.body.appendChild(button);
+
+// Snare Drum Button
+const snareFilter = audioContext.createBiquadFilter();
+snareFilter.type = "highpass";
+snareFilter.frequency.value = 1500; // Measured in Hz
+snareFilter.connect(primaryGainControl);
+
+const snareButton = document.createElement("button");
+snareButton.innerText = "Snare";
+snareButton.addEventListener("click", () => {
+  const whiteNoiseSource = audioContext.createBufferSource();
+  whiteNoiseSource.buffer = whiteNoiseBuffer;
+  whiteNoiseSource.connect(snareFilter);
+
+  whiteNoiseSource.start();
+});
+document.body.appendChild(snareButton);
