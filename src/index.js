b2a68eb117474cdb0ff66d4e0dc2b072ddc3c528
Alex Anderson <alexanderson1993@gmail.com>
2020-11-24 12:04:01 -0500

    Add the white noise button.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import "./styles.css";
+
+const audioContext = new (window.webkitAudioContext || window.AudioContext)();
+const SAMPLE_RATE = audioContext.sampleRate;
+const whiteNoiseTime = 1; // measured in seconds
+
+const whiteNoiseBuffer = audioContext.createBuffer(
+  1,
+  SAMPLE_RATE * whiteNoiseTime,
+  SAMPLE_RATE
+);
+const whiteNoiseChannelData = whiteNoiseBuffer.getChannelData(0);
+for (let i = 0; i < whiteNoiseBuffer.length; i++) {
+  whiteNoiseChannelData[i] = Math.random() * 2 - 1;
+}
+
+// Connect all of our audio nodes to this gain node so their volume is lower.
+const primaryGainControl = audioContext.createGain();
+primaryGainControl.gain.setValueAtTime(0.05, 0);
+primaryGainControl.connect(audioContext.destination);
+
+const button = document.createElement("button");
+button.innerText = "White Noise";
+button.addEventListener("click", () => {
+  const whiteNoiseSource = audioContext.createBufferSource();
+  whiteNoiseSource.buffer = whiteNoiseBuffer;
+  whiteNoiseSource.connect(primaryGainControl);
+
+  whiteNoiseSource.start();
+});
+document.body.appendChild(button);
